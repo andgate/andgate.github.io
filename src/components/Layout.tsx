bbf81e995ca1976b881da22b1686bcaa7347061d
Gabriel Anderson <gabe4k@gmail.com>
2022-01-19 14:15:45 -0800

    adds top margin to footer in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,22 +3,25 @@ import { Header } from './Header'
 import { Footer } from './Footer'
 import { style } from 'typestyle'
 import * as csstips from 'csstips'
+import { px } from 'csx'
 
 const layoutRoot = style(csstips.fillParent, csstips.vertical)
 const layoutHeader = style(csstips.content)
 const layoutContent = style(csstips.flex)
-const layoutFooter = style(csstips.content)
+const layoutFooter = style(csstips.content, { marginTop: px(10) })
 
 export function Layout() {
-  return <div className={layoutRoot}>
-    <div className={layoutHeader}>
-      <Header />
+  return (
+    <div className={layoutRoot}>
+      <div className={layoutHeader}>
+        <Header />
+      </div>
+      <div className={layoutContent}>
+        <Outlet />
+      </div>
+      <div className={layoutFooter}>
+        <Footer />
+      </div>
     </div>
-    <div className={layoutContent}>
-      <Outlet />
-    </div>
-    <div className={layoutFooter}>
-      <Footer />
-    </div>
-  </div>
+  )
 }
